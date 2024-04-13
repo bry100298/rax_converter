0c79ec2caff35d7530a08095a23d70cf7d09dda7
James Tin <jamestin100298@gmail.com>
2024-04-13 16:51:48 +0800

    Feature: remove error checking and update comoany_names for Puregold

--- Puregold/pg.py
+++ Puregold/pg.py
@@ -4,94 +4,88 @@ import pandas as pd
 from xml.etree import ElementTree as ET
 import time
 
+# Variable dictionary mapping company folders to company names
+company_names = {
+    'PPCI': "PUREGOLD PRICE CLUB INC.",
+    'AGRI': "AYAGOLD RETAILERS, INC."
+}
+
 # Function to convert XML to Excel
-def xml_to_excel(xml_file, inbound_folder, outbound_folder, archive_folder, error_folder):
+def xml_to_excel(xml_file, parent_dir):
     # Parse XML file
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    # Check if filename starts with "RA" (case sensitive)
-    if not os.path.basename(xml_file).startswith('RA'):
-        # Move file to Error Folder
-        print(f"Moving '{os.path.basename(xml_file)}' to Error Folder - File name doesn't start with 'RA'")
-        shutil.move(xml_file, os.path.join(error_folder, 'Error'))
-        # shutil.move(xml_file, 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Error')
-        return
+    # Define paths
+    inbound_folder = os.path.join(parent_dir, 'Inbound')
+    outbound_folder = os.path.join(parent_dir, 'Outbound')
+    inbound_outbound_folder = os.path.join(parent_dir, 'Inbound', 'Outbound')
+    archive_folder = os.path.join(parent_dir, 'Archive')
+    error_folder = os.path.join(parent_dir, 'Error')
 
-    # Check if XML file meets conditions
-    company_name = root.find('.//companyName')
-    if company_name is None or company_name.text.strip() != 'SANFORD MARKETING CORPORATION':
-        # Move file to Error Folder
-        print(f"Moving '{os.path.basename(xml_file)}' to Error Folder - Company name is incorrect")
-        shutil.move(xml_file, os.path.join(error_folder, 'Error'))
-        # shutil.move(xml_file, 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Error')
-        return
-
-    # Extract data from XML
+    # Extract data from XML and populate DataFrame
     data = []
-    for article in root.findall('.//article'):
-        trans_code = article.find('transCode').text
+    for invoice_paid in root.findall('.//INVOICES-PAID'):
+        invoice_detail = invoice_paid.find('INVOICE-DETAIL')
+        company_folder = os.path.basename(os.path.dirname(xml_file))
+        EDI_Customer = company_names.get(company_folder, '')
+        ASNAME = root.find('.//ASNAME').text
+        INVOICE_TYPE = invoice_detail.find('INVOICE_TYPE').text
         EDI_TransType = None
-        po_number = article.find('poNumber').text
-        doc_ref = article.find('docRef').text
-        EDI_DocRef = None
-        EDI_DocDesc = None
-        gross_amount = article.find('grossAmount').text
-        EDI_VAT = None
-        EDI_EWT = None
-        netAmount = article.find('netAmount').text
-        check_number = root.find('.//checkNumber').text
-        check_date = root.find('.//checkDate').text
-        #netPayable = root.find('//netPayable').text
+        AIINV = invoice_detail.find('AIINV').text
+        AIAMT = invoice_detail.find('AIAMT').text
+        WITHHOLDINGTAX = invoice_paid.find('.//DEDUCTIONS/WITHHOLDINGTAX').text
+        AITONT = invoice_detail.find('AITONT').text
+        AICHQ = invoice_detail.find('AICHQ').text
+        BATCH_AMOUNT = invoice_detail.find('BATCH_AMOUNT').text
 
-        # Extract netPayable directly from the root
-        netPayable_elem = root.find('.//netPayable')
-        netPayable = netPayable_elem.text if netPayable_elem is not None else None
-
-        # Append to data list
-        data.append([trans_code, EDI_TransType, po_number, doc_ref, EDI_DocRef, EDI_DocDesc, gross_amount, EDI_VAT, EDI_EWT, netAmount, check_number, check_date, netPayable])
+        data.append([EDI_Customer, ASNAME, INVOICE_TYPE, EDI_TransType, PONUMB, AIINV, AIAMT, None, WITHHOLDINGTAX, AITONT, AICHQ, None, BATCH_AMOUNT])
 
     # Create DataFrame
-    df = pd.DataFrame(data, columns=['EDI_DocType', 'EDI_TransType', 'EDI_PORef', 'EDI_InvRef', 'EDI_DocRef', 'EDI_DocDesc', 'EDI_Gross', 'EDI_VAT', 'EDI_EWT', 'EDI_Net', 'EDI_RARef', 'EDI_RADate', 'EDI_RAAmt'])
+    df = pd.DataFrame(data, columns=['EDI_Customer', 'EDI_Company', 'EDI_DocType', 'EDI_TransType', 'EDI_PORef', 'EDI_InvRef', 'EDI_Gross', 'EDI_Discount', 'EDI_EWT', 'EDI_Net', 'EDI_RARef', 'EDI_RADate', 'EDI_RAAmt'])
 
     # Create Excel file path
-    excel_file = os.path.join(outbound_folder, os.path.basename(xml_file).replace('.xml', '.xlsx'))
+    company_folder = os.path.basename(os.path.dirname(xml_file))
+    excel_folder = os.path.join(inbound_outbound_folder, company_folder)
+    os.makedirs(excel_folder, exist_ok=True)
+    excel_file = os.path.join(excel_folder, os.path.basename(xml_file).replace('.xml', '.xlsx'))
 
     # Write DataFrame to Excel
     df.to_excel(excel_file, index=False)
 
     # Create Archive Folder if not exists
-    archive_excel_folder = os.path.join(archive_folder, 'excel')
-    if not os.path.exists(archive_excel_folder):
-        os.makedirs(archive_excel_folder)
+    archive_excel_folder = os.path.join(archive_folder, 'excel', company_folder)
+    os.makedirs(archive_excel_folder, exist_ok=True)
+
+    # Create Archive Folder if not exists
+    archive_xml_folder = os.path.join(archive_folder, 'xml', company_folder)
+    os.makedirs(archive_xml_folder, exist_ok=True)
 
     # Copy Excel file to Archive excel Folder
     shutil.copy(excel_file, os.path.join(archive_excel_folder, os.path.basename(excel_file)))
 
     # Move XML file to Archive xml Folder
-    shutil.move(xml_file, os.path.join(archive_folder, 'xml'))
+    shutil.move(xml_file, os.path.join(archive_xml_folder, os.path.basename(xml_file)))
 
     # Move Excel file to Outbound Folder
-    shutil.move(excel_file, 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Outbound')
+    shutil.move(excel_file, os.path.join(outbound_folder, company_folder))
 
 # Main function
 def main():
-    # Path settings
-    inbound_folder = 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Inbound'
-    outbound_folder = 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Inbound/Outbound'
-    archive_folder = 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Archive'
-    error_folder = 'C:/Users/User/Documents/Project/rax_converter/SM_Group'
-    #error_folder = 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Error' #if use this , the filename will trigger instead of folder shutil.move(xml_file, os.path.join(error_folder, 'xml'))
+    # Get parent directory of the script
+    # script_dir = os.path.dirname(__file__)
+    # parent_dir = os.path.join(script_dir, 'SM_Group')
+    parent_dir = 'Metro_Gaisano'
 
     # Iterate over XML files in Inbound Folder
-    for file in os.listdir(inbound_folder):
-        if file.endswith('.xml'):
-            xml_file = os.path.join(inbound_folder, file)
-            xml_to_excel(xml_file, inbound_folder, outbound_folder, archive_folder, error_folder)
+    inbound_dir = os.path.join(parent_dir, 'Inbound')
+    for root_folder, dirs, files in os.walk(inbound_dir):
+        for file in files:
+            if file.endswith('.xml'):
+                xml_file = os.path.join(root_folder, file)
+                xml_to_excel(xml_file, parent_dir)
 
-            # Move Excel file to Outbound Folder
-            #shutil.move(os.path.join(outbound_folder, os.path.basename(xml_file).replace('.xml', '.xlsx')), 'C:/Users/User/Documents/Project/rax_converter/SM_Group/Outbound')
-        time.sleep(15)  # Wait for 15 seconds before processing next file
+    time.sleep(5)  # Wait for 5 seconds before exiting
 
 if __name__ == "__main__":
     main()
