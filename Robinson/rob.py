1afdceaf5d268cf94ccff83dce393ba22c2fb272
James Tin <jamestin100298@gmail.com>
2024-04-17 08:31:51 +0800

    Feature: Fix

--- Robinson/rob.py
+++ Robinson/rob.py
@@ -28,44 +28,6 @@ def move_to_archive(source_folder, target_folder):
             target_file = os.path.join(target_folder, file)
             shutil.move(source_file, target_file)
 
-def generate_excel(company_folder, source_folder, target_folder):
-    # Read the merged Excel file
-    excel_file = os.path.join(source_folder, f"opadosopd_{datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx")
-    df = pd.read_excel(excel_file)
-    
-    # Define the mapping of fields
-    field_mapping = {
-        "EDI_Customer": lambda x: company_names.get(company_folder, ''),
-        "EDI_Company": "VENDOR",
-        "EDI_DocType": "Transaction_Type",
-        "EDI_TransType": None,
-        "EDI_PORef": "PO Number.",
-        "EDI_InvRef": "Invoice No",
-        "EDI_Gross": "RC Amount",
-        "EDI_Discount": None,
-        "EDI_EWT": "EWT",
-        "EDI_Net": "NET AMOUNT",
-        "EDI_RARef": "Payment Ref No",
-        "EDI_RADate": "PAYMENT_DATE",
-        "EDI_RAAmt": "Cheque Amount"
-    }
-    
-    # Apply the field mapping
-    for field, column in field_mapping.items():
-        if column is not None:
-            df[field] = df[column]
-        else:
-            df[field] = None
-    
-    # Create the target directory if it doesn't exist
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
-    
-    # Save the DataFrame to a new Excel file
-    save_path = os.path.join(target_folder, f"{company_folder}_EDI_{datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx")
-    df.to_excel(save_path, index=False)
-    print(f"Excel file generated for {company_folder}: {save_path}")
-
 def merge_excel_files_robd(company_code):
     company_folder = company_names.get(company_code)
     if company_folder is None:
@@ -115,9 +77,6 @@ def merge_excel_files_robd(company_code):
     move_to_archive(inbound_folder, archive_folder)
     print("Original files moved to archive.")
 
-    # Generate Excel file with specific fields
-    # generate_excel("ROBD", "C:/Users/User/Documents/Project/rax_converter/Robinson/Inbound/Merged/ROBD", "C:/Users/User/Documents/Project/rax_converter/Robinson/Inbound/Outbound/ROBD")
-
 def merge_excel_files_robs(company_code):
     company_folder = company_names.get(company_code)
     if company_folder is None:
@@ -166,9 +125,6 @@ def merge_excel_files_robs(company_code):
     # Move original files to archive
     move_to_archive(inbound_folder, archive_folder)
     print("Original files moved to archive.")
-
-    # Generate Excel file with specific fields
-    # generate_excel("ROBS", "C:/Users/User/Documents/Project/rax_converter/Robinson/Inbound/Merged/ROBS", "C:/Users/User/Documents/Project/rax_converter/Robinson/Inbound/Outbound/ROBS")
 
 # Call the functions to execute the merging process
 merge_excel_files_robd('ROBD')
